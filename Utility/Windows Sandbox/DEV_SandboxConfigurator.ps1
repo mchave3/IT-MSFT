66a0b500ff8e8ae64b90ff58b181e83a6a313b96
Mickaël CHAVE <38375531+mchave3@users.noreply.github.com>
2024-09-12 14:50:28 +0200

    update

---
+++ Utility/Windows Sandbox/DEV_SandboxConfigurator.ps1
@@ -0,0 +1,17 @@
+<#
+.SYNOPSIS
+Windows Sandbox Configurator
+
+.DESCRIPTION
+This script provides a graphical user interface (GUI) for configuring and managing Windows Sandbox. It allows users to install Windows Sandbox, save and load sandbox configurations, and start the sandbox with a specific configuration file.
+
+.NOTES
+Author: Mickaël CHAVE
+Date: 12/09/2024
+Version: 1.0
+#>
+
+Clear-Host
+
+Add-Type -AssemblyName System.Windows.Forms
+
